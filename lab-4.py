0b2611c24862617fee2033a063477d31da69c7de
gh-vdg <gh-vdg@users.noreply.github.com>
2022-03-26 01:11:01 -0400

    Update lab-4.py
    code revision on lab-4

--- lab-4.py
+++ lab-4.py
@@ -8,11 +8,24 @@ def syncTables(event, context):
 # Scan returns paginated results, so only partial data will be copied
 def sync_ddb_table(source_ddb, destination_ddb):
     response = source_ddb.scan(
-        TableName="<FMI1>"
+        TableName="dragon_bonus_attack"
     )
 
+   if LastEvaluateKey:
+         paginator = client.get_paginator('list_objects')
+   #      operation_parameters = ('TableName': 'dragon_bonus_attack')
+         page_iterator = paginator.paginate(**operation_parameters)
+
+    
     for item in response['Items']:
         destination_ddb.put_item(
-            TableName="<FMI2>",
+            TableName="dragon_table_west",
             Item=item
+        )
+           
+        
+
+for page in page_iterator:
+    print(page['Contents'])
+        
         )
